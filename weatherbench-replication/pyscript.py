c917e82570e52705c58efa2d7c349f8cee1e07f8
Seongbin Park <shannonsbpark@gmail.com>
2022-10-16 18:33:07 -0700

    Added script for download + first attempt at replication

---
+++ weatherbench-replication/pyscript.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+path = Path("/u/scratch/s/se0ngbin/weatherbench")
+
+from climate_tutorial.data import download
+
+download(root = path, source = "weatherbench", variable = "2m_temperature", dataset = "era5", resolution = "5.625")
+download(root = path, source = "weatherbench", variable = "geopotential_500", dataset = "era5", resolution = "5.625")
+download(root = path, source = "weatherbench", variable = "temperature_850", dataset = "era5", resolution = "5.625")
+download(root = path, source = "weatherbench", variable = "geopotential", dataset = "era5", resolution = "5.625")
+download(root = path, source = "weatherbench", variable = "temperature", dataset = "era5", resolution = "5.625")
+
+
